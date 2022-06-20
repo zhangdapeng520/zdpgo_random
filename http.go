86110be90f274a3f7a4786cff89e6c3aa9d38827
张大鹏 <pygosuperman@outlook.com>
2022-06-20 16:03:34 +0800

    升级：日志组件升级

--- http.go
+++ http.go
@@ -8,19 +8,19 @@ import (
 func (r *Random) HttpPort() int {
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
 	if err != nil {
-		Log.Error("解析TCP地址失败", "error", err)
+		r.Log.Error("解析TCP地址失败", "error", err)
 		return 0
 	}
 
 	l, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		Log.Error("创建tcp监听失败", "error", err)
+		r.Log.Error("创建tcp监听失败", "error", err)
 		return 0
 	}
 	defer l.Close()
 
 	// 获取端口号
 	p := l.Addr().(*net.TCPAddr).Port
-	Log.Debug("获取可用的HTTP端口号成功", "port", p)
+	r.Log.Debug("获取可用的HTTP端口号成功", "port", p)
 	return p
 }
